64b4653b184c3ea7a10ae7b79295b224f30a3255
andrea <mrblasco@gmail.com>
2015-05-22 09:59:34 -0400

    Add electorate

--- extract_data.sh
+++ extract_data.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 ## Download Tabelle
-cd data/tabelle
+ cd data/tabelle
 curl -O http://amministratori.interno.it/tabelle/tabella[6-10].txt 
 curl -O http://amministratori.interno.it/tabelle/codicicarica.txt
 
 cd ../comuni
-curl -O http://amministratori.interno.it/amministratori/storico/comuni3112[1985-2014].zip
+curl -O http://amministratori.interno.it/amministratori/storico/comuni3112[1985-2015].zip
 
 cd ../province
-curl -O http://amministratori.interno.it/amministratori/storico/province3112[1985-2014].zip
+curl -O http://amministratori.interno.it/amministratori/storico/province3112[1985-2015].zip
 
 cd ../regioni
-curl -O http://amministratori.interno.it/amministratori/storico/regioni3112[1985-2014].zip
+curl -O http://amministratori.interno.it/amministratori/storico/regioni3112[1985-2015].zip
+
+# Corpo elettorale
+cd ../electorate
+curl -O http://amministratori.interno.it/semestrale/seme/3006[1988-2015].zip
+curl -O http://amministratori.interno.it/semestrale/seme/3112[1988-2015].zip
