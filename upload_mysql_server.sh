1b8728782c92b2bc16d7b41d1265cf36cb0705ad
andrea <mrblasco@gmail.com>
2015-06-01 08:32:56 -0400

    improved bash

--- upload_mysql_server.sh
+++ upload_mysql_server.sh
@@ -8,21 +8,19 @@ function anagrafe {
    /Applications/MAMP/Library/bin/mysql --user=$user --password=$passw --database=$database -e "$1" 
 } 
 
-
+anagrafe "TRUNCATE anagrafe.comuni"
 for f in data/comuni/*.zip
 do
    echo "Unzipping..."$f
    unzip $f
-done
-
-for f in *.txt
-do
-   line=$(head -n 1 $f)
-   line=${line//;/,}
-   echo "Adding..."$f
-   anagrafe "LOAD DATA LOCAL INFILE '"$f"'INTO TABLE anagrafe.comuni COLUMNS TERMINATED BY ';' LINES TERMINATED BY '\r' IGNORE 1 LINES ($line);"
+   filename=$(basename $f)
+   filename=${filename%.*}.txt
+   fields=$(head -n 1 $f)
+   fields=${fields//;/,}
+   echo "Adding..."$filename
+   anagrafe "LOAD DATA LOCAL INFILE '"$filename"'INTO TABLE anagrafe.comuni COLUMNS TERMINATED BY ';' LINES TERMINATED BY '\r' IGNORE 1 LINES ($fields);"
    anagrafe "SELECT count(*) from comuni"
-   rm $f
-done
+   rm $filename
+# done
 
 
